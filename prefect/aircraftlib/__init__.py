7252402f541bc2b53359fd01e4616c5c5376e030
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-08-03 11:26:29 -0600

    update pre-commit hooks

--- prefect/aircraftlib/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa
-from .analysis import add_airline_info, clean_vector
-from .database import Database
-from .openflights import fetch_reference_data
-from .opensky import fetch_live_aircraft_data
-from .position import Area, Position, bounding_box
